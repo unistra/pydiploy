df6ad3ba9172300ef5145b1cd364cd4e9c6f15cb
Arnaud Grausem <arnaud.grausem@gmail.com>
2012-09-21 22:58:47 +0200

    git archive

---
+++ src/pydiploy/git.py
@@ -0,0 +1,18 @@
+"""
+"""
+
+
+from fabric.api import local
+
+
+def archive(filename, tag="HEAD", remote="", prefix=""):
+    """
+    """
+    options = ['--format=tar']
+    if remote:
+        options.append('--remote=%s' % remote)
+    if prefix:
+        options.append('--prefix=%s' % prefix)
+
+    options_build = ' '.join(options)
+    local('git archive %s %s |gzip > %s' % (options_build, tag, filename))
