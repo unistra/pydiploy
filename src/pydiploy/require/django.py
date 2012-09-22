d22af6af9dabd0a192fc37285ac038ffd424419a
Arnaud Grausem <arnaud.grausem@gmail.com>
2012-09-22 16:54:54 +0200

    some require tools for django

--- src/pydiploy/require/django.py
+++ src/pydiploy/require/django.py
@@ -2,22 +2,58 @@
 """
 
 
+from fabric.api import cd
 from fabric.api import env
-from fabric.api import require
+from fabric.api import get
+from fabric.api import require as req
 from fabric.api import run
 
+import fabtools
+from fabtools import require
 
-def base_conf(remote_basedir, project_name, virtualhost, media_root):
+
+def base_conf(media_root):
     """
     """
-    env.remote_basedir = remote_basedir
-    env.remote_workdir = '%(remote_basedir)s/%(virtualhost)s' % locals()
-    env.remote_logdir = '%(remote_basedir)s' % locals()
-    env.django_settings_module = '%(project_name)s.settings' % locals()
-    env.django_media_root = '%(media_root)s/%(virtualhost)s' % locals()
+    env.remote_workdir = '%(remote_basedir)s/%(virtualhost)s' % env
+    env.remote_logdir = '%(remote_workdir)s/log' % env
+    env.django_settings_module = '%(project_name)s.settings' % env
+    env.django_media_root = '%s/%s' % (media_root, env.virtualhost)
 
 
-def copy_manager(temp_dir):
-    require('project_name')
-    run('cp %s/src/%s/manager.py %s' % (temp_dir, env.project_name,
-        env.remote_workdir))
+def manager(temp_dir):
+    req('project_name, remote_workdir')
+    with cd(env.remote_workdir):
+        run('cp %s/src/%s/manage.py .' % (temp_dir, env.project_name))
+
+
+def staticfiles(temp_dir):
+    req('project_name, remote_workdir')
+    with cd(env.remote_workdir):
+        run('cp -R %s/src/%/static _static' % (temp_dir, env.project_name))
+        run('python manage.py collectstatic -c -l --noinput')
+
+
+def locales(temp_dir):
+    req('project_name, remote_workdir')
+    with cd(env.remote_workdir):
+        run('cp -R %s/src/%s/locale .' % temp_dir, env.project_name)
+        run('python manage.py compilemessages')
+
+
+def settings(temp_dir, context):
+    req('project_name, remote_workdir')
+    with cd(temp_dir):
+        get('conf/local_settings.py.template', '/tmp/local_settings.py')
+    require.files.template_file(
+            path='%s/src/%s/local_settings.py' % (temp_dir, env.project_name),
+            template_source='/tmp/local_settings.py', context=context)
+
+
+def db_engines():
+    """
+    """
+    with fabtools.python.virtualenv(env.virtualenv_dir):
+        from django.conf.settings import DATABASES
+        return [database['ENGINE'].split('.')[-1] for database 
+                in DATABASES.itervalues()]
