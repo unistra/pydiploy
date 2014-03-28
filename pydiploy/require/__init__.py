9717c79193cef0dbdd9ef790a1f23b856129777a
Thomas Baguet <baguet@unistra.fr>
2014-03-28 15:55:59 +0100

    fix bad import

--- pydiploy/require/__init__.py
+++ pydiploy/require/__init__.py
@@ -6,4 +6,5 @@ from pydiploy.require import nginx
 from pydiploy.require import circus
 from pydiploy.require import python
 from pydiploy.require import system
+from pydiploy.require import git
 from pydiploy.require import releases_manager
