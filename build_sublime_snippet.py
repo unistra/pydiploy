730657e0d94cc937c86b134ba3f4da0d91597197
Morgan <morgan.bohn@unistra.fr>
2015-12-31 11:01:55 +0100

    add sublime snippet for simple app

--- build_sublime_snippet.py
+++ build_sublime_snippet.py
@@ -23,8 +23,22 @@ def copyFile(src, dest):
         print('Error: %s' % e.strerror)
 
 
-def main():
+def install_snippet(dest_filename, app_type):
+    """ install snippets """
+    # TODO manage other os (Darwin,windoz...)
+    if platform.system() == "Linux":
+        sublime_config_dirs = [
+            '.config/sublime-text-3/Packages/User',
+            '.config/sublime-text-2/Packages/User']
+        for dir in sublime_config_dirs:
+            dir = os.path.join(os.environ['HOME'], dir)
+            if os.path.isdir(dir):
+                copyFile(dest_filename, dir)
+                print('Snippet for %s app installed in %s' % (app_type, dir))
 
+
+def django_app():
+    # Django snippets
     src_filename = 'pydiploy/examples/django_fabfile.py'
     dest_filename = 'tools/pydiployfabfile.sublime-snippet'
 
@@ -40,16 +54,34 @@ def main():
     source.close()
     target.close()
 
-    # TODO manage other os (Darwin,windoz...)
-    if platform.system() == "Linux":
-        sublime_config_dirs = [
-            '.config/sublime-text-3/Packages/User',
-            '.config/sublime-text-2/Packages/User']
-        for dir in sublime_config_dirs:
-            dir = os.path.join(os.environ['HOME'], dir)
-            if os.path.isdir(dir):
-                copyFile(dest_filename, dir)
-                print('Snippet installed in %s' % dir)
+    install_snippet(dest_filename, "django")
+
+
+def simple_app():
+    # Simple snippets
+    src_filename = 'pydiploy/examples/simple_fabfile.py'
+    dest_filename = 'tools/pydiployfabfilesimple.sublime-snippet'
+
+    source = open(src_filename, 'r')
+    target = open(dest_filename, 'w')
+
+    snippet_header = '<snippet>\n<content><![CDATA[\n'
+    snippet_footer = '\n]]></content>\n<description>Pydiploy fabfile simple %s</description>\n<tabTrigger>pydiployfabsimple</tabTrigger>\n<scope>source.python</scope>\n</snippet>' % __version__
+
+    target.write(snippet_header)
+    target.write(source.read())
+    target.write(snippet_footer)
+    source.close()
+    target.close()
+
+    install_snippet(dest_filename, "simple")
+
+
+def main():
+    django_app()
+    simple_app()
+
+
 
 
 if __name__ == '__main__':
