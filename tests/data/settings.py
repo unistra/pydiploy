36b0ead6bcfea59caa1f274f5c069f3e5c996480
Morgan <morgan.bohn@unistra.fr>
2015-12-31 10:24:25 +0100

    simple app: add unit tests

--- tests/data/settings.py
+++ tests/data/settings.py
@@ -3,5 +3,7 @@ ADMINS = (
 )
 
 SECRET_KEY = '{{ secret_key }}'
+SERVER_URL = '{{ server_url }}'
 
+DATABASES = {'default': {'USER': None}}# initialize dict
 DATABASES['default']['USER'] = '{{ default_db_user }}'
