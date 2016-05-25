d79bde766d915f2470a84a876aeaff14add73048
Thomas Baguet <baguet@unistra.fr>
2016-05-25 09:56:30 +0200

    Add autoupgrade of pip in venv

--- pydiploy/require/python/utils.py
+++ pydiploy/require/python/utils.py
@@ -30,8 +30,8 @@ def application_dependencies(upgrade_pkg, staging=True):
             requirements_file = os.path.join('requirements',
                                              '%s.txt' % env.goal) if staging else 'requirements.txt'
             # ugly fix for error when pip install fail and error raises while /home/user/.pip not writable
-            pip_log = "%s/pip_error.log " % env.remote_home
-            pip_cmd = 'pip --no-cache-dir --log-file %s' % pip_log
+            pip_log = '%s/pip_error.log' % env.remote_home
+            pip_cmd = 'pip --log-file %s' % pip_log
             if 'oracle_client_version' in env:
                 oracle_dir = 'instantclient_%s' % '_'.join(
                     env.oracle_client_version.split('.')[:2])
@@ -41,11 +41,19 @@ def application_dependencies(upgrade_pkg, staging=True):
                     env.remote_home, oracle_root_path)
                 pip_cmd = 'ORACLE_HOME=%s pip' % oracle_full_path
 
+            # upgrade pip to latest version
+            fabtools.require.python.install('pip', 
+                                            upgrade=True, 
+                                            use_sudo=True,
+                                            user=env.remote_owner,
+                                            pip_cmd=pip_cmd,
+                                            quiet=True)
+
             fabtools.python.install_requirements(requirements_file,
                                                  use_sudo=True,
                                                  user=env.remote_owner,
                                                  upgrade=upgrade_pkg,
-                                                 pip_cmd=pip_cmd,
+                                                 pip_cmd='%s --no-cache-dir' % pip_cmd,
                                                  quiet=True)
 
             fabric.api.sudo(
