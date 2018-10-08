ebdfcc23a11009210be9cd58df3502bfe4f8adb9
Thomas Baguet <baguet@unistra.fr>
2018-10-08 14:17:40 +0200

    Monkey patch for vistualenv creation when using sudo

--- pydiploy/require/python/virtualenv.py
+++ pydiploy/require/python/virtualenv.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """ This module is used for virtualenv relatives """
-
+from contextlib import contextmanager
 import fabtools
 from fabric.api import env
 from pydiploy.decorators import do_verbose
+
+
+@contextmanager
+def shell(new_shell):
+    old_shell, env.shell = env.shell, new_shell
+    yield
+    env.shell = old_shell
 
 
 @do_verbose
@@ -15,7 +22,8 @@ def virtualenv(clear=False):
                                      group=env.remote_group,
                                      use_sudo=True)
     python_bin = '/usr/bin/python%s' % env.remote_python_version
-    fabtools.require.python.virtualenv(env.remote_virtualenv_dir,
-                                       user=env.remote_owner,
-                                       clear=clear, use_sudo=True,
-                                       venv_python=python_bin)
+    with shell("HOME=~%s %s" % (env.remote_owner, env.shell)):
+        fabtools.require.python.virtualenv(env.remote_virtualenv_dir,
+                                           user=env.remote_owner,
+                                           clear=clear, use_sudo=True,
+                                           venv_python=python_bin)
